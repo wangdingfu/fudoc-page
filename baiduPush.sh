de951289fd732bf70ac2bd0dbd4cbb38e37207b3
wangdingfu <fuge@douyu.tv>
2023-08-15 11:49:36 +0800

    推送百度token修改

--- baiduPush.sh
+++ baiduPush.sh
@@ -4,6 +4,6 @@
 set -e
 
 # 百度链接推送
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=http://www.fudoc.cn&token=GHmhewheYhbxG6Zq"
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=http://www.fudoc.cn&token=u4Rwbm6wEVtRGyG3"
 
 rm -rf urls.txt # 删除文件
